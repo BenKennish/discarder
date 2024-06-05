dbd7134118df3a2118c4ceffca45dc1aca730200
Ben Kennish <ben@kennish.net>
2024-06-05 13:16:20 +0100

    Use Promise.all with an array of discardTasks

--- popup.js
+++ popup.js
@@ -9,63 +9,72 @@ document.addEventListener('DOMContentLoaded', function ()
     let numTabsDiscarded = 0;
     let numTabsFailedToDiscard = 0;
 
-    return new Promise((resolve) =>
+    const discardTasks = [];
+
+    chrome.windows.getAll({populate: true}).then((windows) =>
     {
-      chrome.windows.getAll({populate: true}).then((windows) =>
+
+      for (let window of windows)
       {
-
-        for (let window of windows)
+        for (let tab of window.tabs)
         {
-          for (let tab of window.tabs)
+          if (!tab.discarded && (force || (tab.autoDiscardable && !tab.active)))
           {
-            if (!tab.discarded && (force || (tab.autoDiscardable && !tab.active)))
-            {
-              // tab.active means that a tab is active within its window
-              // seems to be true for tabs that are selected in a window that is partially visible
+            // tab.active means that a tab is active within its window
+            // seems to be true for tabs that are selected in a window that is partially visible
 
-              //TODO: calculate RAM usage
+            numTabsToDiscard++;
 
-              numTabsToDiscard++;
+            console.log('tab.memoryInfo', tab.memoryInfo);
 
-              // without shift-click we only discard
-              // auto-discardable tabs that aren't the active tab
-              chrome.tabs.discard(tab.id).then( (t) => {
+            // without shift-click we only discard
+            // auto-discardable tabs that aren't the active tab
 
-                if (t) { // we discarded the tab successfully
-                  console.log(`Tab ${tab.id} discarded`);
+            discardTasks.push(
+
+              chrome.tabs.discard(tab.id).then( (discardedTab) => {
+
+                if (discardedTab) { // we discarded the tab successfully
+                  //console.log(`Tab ${discardedTab.id} discarded`);
                   numTabsDiscarded++;
+                
+                  console.log('discardedTab.memoryInfo', discardedTab.memoryInfo);
+
+                  //TODO: calculate RAM usage
+                  return true;
                 }
                 else {
                   console.warn(`Error discarding tab ${tab.id}:`, chrome.runtime.lastError);
                   numTabsFailedToDiscard++;
+                  return false;
                 }
 
-              });
+              })
 
-            }
+            );
+
           }
         }
 
-        //numTabsToDiscard is now correct
+      }; //chrome.windows.getAll
 
-        // 1. wait until numTabsDiscarded +  == numTabsToDiscard? how?
-        // 2. display a message giving the number of tabs discarded and the MB ram freed (approx)
+      Promise.all(discardTasks).then( (results) => {
+
+        console.log('results', results);
 
         chrome.notifications.create(
         {
            type: "basic",
            iconUrl: "icon128.png",
            title: "Tabs Discarded",
-           message: numTabsToDiscard+' tabs were marked for discarding',
+           message: `${numTabsDiscarded} tabs were discarded, ${numTabsFailedToDiscard} failed to discard`,
            priority: 0,
            silent: true
         });
-
-      }); //chrome.windows.getAll
-
-      resolve(true);
+      });
 
     });
+
   }
 
 
