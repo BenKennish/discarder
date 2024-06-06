4a1d0c6e026b4d8b0c369417faed94ccf5ba23c8
Ben Kennish <ben@kennish.net>
2024-06-06 21:06:56 +0100

    When force, discard active tabs too

--- popup.js
+++ popup.js
@@ -2,10 +2,12 @@
 // human readable form
 function bytesToSize(bytes) 
 {
-  //TODO: show only 1 or 2 decimal places
+  //TODO: show only 1 or 2 decimal places?
   const units = ["byte", "kilobyte", "megabyte", "gigabyte", "terabyte"];
   const unitIndex = Math.max(0, Math.min(Math.floor(Math.log(bytes) / Math.log(1024)), units.length - 1));
-  return Intl.NumberFormat("en", { style: "unit", unit: units[unitIndex] }).format(bytes / (1024 ** unitIndex));
+  return Intl.NumberFormat("en", { style: "unit", unit: units[unitIndex], maximumFractionDigits: 2 }).format(
+    bytes / (1024 ** unitIndex)
+  );
 }
 
 
@@ -15,26 +17,28 @@ document.addEventListener('DOMContentLoaded', function ()
   const discardButton = document.getElementById('discardButton');
 
   // this is a shit way of doing it as it's affected by other system processes etc
+  // but it's the best i've figured out for now
   let memAvailableBefore = 0;
   let memAvailableAfter = 0;
 
-  function discardTabs(force) {
+  function discardTabs(force)
+  {
 
     const discardTasks = [];
 
     chrome.windows.getAll({populate: true}).then((windows) =>
     {
-
       for (let window of windows)
       {
         for (let tab of window.tabs)
         {
-          if (!tab.discarded && (force || (tab.autoDiscardable && !tab.active)))
+          if (!tab.discarded && (force || (!tab.active && tab.autoDiscardable)))
           {
-            // without shift-click we only discard
-            // auto-discardable tabs that aren't active
+            // without shift-click we ignore tabs that aren't auto-discardable
             //
             // active means that a tab is active within its window if window is partially visible
+            // it seems that discarding active tabs is kinda pointless because they are just automatically
+            // reloaded anyway
 
             // chrome.processes API is not available for non nightly builds so none of this works
             //let pid = chrome.processes.getProcessIdForTab(tab.id);
@@ -49,10 +53,6 @@ document.addEventListener('DOMContentLoaded', function ()
                 {
                   // we discarded the tab successfully
                   console.log(`Tab ${discardedTab.id} discarded`);
-                  console.log(discardedTab);
-
-                  //TODO: calculate RAM usage here?
-
                   return true;
                 }
                 else
@@ -60,12 +60,11 @@ document.addEventListener('DOMContentLoaded', function ()
                   console.warn(`Error discarding tab ${tab.id}:`, chrome.runtime.lastError);
                   return false;
                 }
-
               })
             );
           }
         }
-      }; //chrome.windows.getAll()
+      };
 
       Promise.all(discardTasks).then((results) => {
 
